0c6b79f2da9a960ce6ba93ea712210f2294dd578
keet <wwukuww@gmail.com>
2019-09-09 14:03:01 +0900

    shape interface 가져 왔습니다.

---
+++ myInterface/Shape.java
@@ -0,0 +1,20 @@
+package myInterface;
+
+/**
+ * インターフェース Shape のための注釈をここに書きます.
+ * 
+ * @author (あなたの名前) 
+ * @version (バージョン番号もしくは日付)
+ */
+
+public interface Shape{
+    final double PI = 3.14;
+    void draw();
+
+    double getArea();
+
+    default public void redraw(){
+        System.out.print("---다시 그립니다.");
+        draw();
+    }
+}
