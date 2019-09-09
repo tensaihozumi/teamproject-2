7fbd9d624dcf223c396f34aeccca4b7d1874553b
otaoaki <taiaki1102@gmail.com>
2019-09-09 20:31:48 +0900

    アップデート

--- main/Myapp.java
+++ main/Myapp.java
@@ -9,27 +9,6 @@ package main;
  */
 public class Myapp
 {
-    // インスタンス変数 - コードに合わせて説明を書き換えます.
-    private int x;
-
-    /**
-     * Myapp クラスのインスタンスのためのコンストラクタ
-     */
-    public Myapp()
-    {
-        // インスタンス変数の初期化
-        x = 0;
-    }
-
-    /**
-     * メソッドの例 - メソッドとともにこのコメントも置き換えてください.
-     * 
-     * @param  y    メソッドのためのサンプル引数
-     * @return        x と y の和
-     */
-    public int sampleMethod(int y)
-    {
-        // ここにあなたのコードを書きます
-        return x + y;
-    }
+    
+     
 }
