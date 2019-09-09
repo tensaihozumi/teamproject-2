35db873dd2896a25abbe167d62b8fc9eb7ebb752
otaoaki <taiaki1102@gmail.com>
2019-09-09 21:06:26 +0900

    myapp의 고드를 만들었다

---
+++ base/Circle.java
@@ -0,0 +1,15 @@
+package base;
+
+
+/**
+ * 여기에 Circle 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Circle
+{
+   int x,y;
+   
+   
+}
--- main/Myapp.java
+++ main/Myapp.java
@@ -1,4 +1,4 @@
-package main;
+ package main;
 
 
 /**
@@ -9,6 +9,16 @@ package main;
  */
 public class Myapp
 {
-    
-     
+    public static void main(String[] args){
+        Circle c1 = new Circle(2,3,5);
+        Circle c2 = new Circle(2,3,10);
+        System.out.println("원1:" + c1);
+        System.out.println("원2:" + c2);
+        if(c1.equals(c2))
+            System.out.println("같은 원");
+        else
+            System.out.println("서로 다른 원");
+          
+        
+    }
 }
